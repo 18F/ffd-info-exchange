e6dbe0ff676ba6ea03639565264f3c9d1a5748ca
Fureigh <rhys.fureigh@gsa.gov>
2016-10-27 12:04:37 -0700

    Add whitenoise to wsgi.py file
    Per https://cloud.gov/docs/apps/django/

--- ffd_info_exchange/ffd_info_exchange/wsgi.py
+++ ffd_info_exchange/ffd_info_exchange/wsgi.py
@@ -12,5 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ffd_info_exchange.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
