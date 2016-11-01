d3b51c235b5a974587c50abd73cdc3f295e37eb0
Fureigh <rhys.fureigh@gsa.gov>
2016-11-01 07:27:40 -0400

    Add more sections, start making multi-step
    Includes 'Student eligibility' through 'Parent financial information, continued'. Displays only the first step of the multi-step form at this point.
    
    github.com/18F/ffd-info-exchange/issues/26

--- ffd_info_exchange/fafsa/views.py
+++ ffd_info_exchange/fafsa/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import FAFSAApplicationForm
+from .forms import *
 #from .models import Application
 
 
 def fafsa_form(request):
 #    form = FAFSAApplicationForm(request.POST or None)
-    form = FAFSAApplicationForm
+    form = FAFSAApplicationForm1
 #    if form.is_valid():
 #        application = form.save()
 #        return redirect(confirmation_screen)
