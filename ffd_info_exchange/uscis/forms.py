c74ea8bd8a529db4f1e3363f442052526b1cb314
Fureigh <rhys.fureigh@gsa.gov>
2016-11-30 17:26:18 -0800

    Flesh out 'Your eligibility' and 'About you'
    ...per https://github.com/18F/ffd-info-exchange/issues/100.
    
        https://github.com/18F/ffd-info-exchange/issues/98

--- ffd_info_exchange/uscis/forms.py
+++ ffd_info_exchange/uscis/forms.py
@@ -7,43 +7,95 @@ from localflavor.us.us_states import STATE_CHOICES
 #from django_countries.widgets import CountrySelectWidget
 
 BLANK_CHOICE = (('', 'Choose one'))
-# @maybe: Check how the N-400 handles nonbinary genders. The passport office
-# doesn't issue documents with them but does accept documents with them.
+YES_OR_NO = (BLANK_CHOICE, ('1', 'Yes'), ('0', 'No'))
+YES_NO_MAYBE = (BLANK_CHOICE, ('1', 'Yes'), ('0', 'No'), ('2', 'Maybe'))
+ELIGIBILITY_OPTIONS = (('0', "I’ve been a lawful permanent resident for at least five years."),
+                       ('1', "I’ve been a lawful permanent resident for at least three years and I’ve been married to my spouse (a U.S. citizen) for at least three years. (As of the date I applied for citizenship, my spouse has been a U.S. citizen for at least three years.)"),
+                       ('2', "I’ve been a lawful permanent resident for at least three years, I have a spouse who’s a U.S. citizen, and my spouse frequently participates in specified employment abroad."),
+                       # @todo: Add help text for the above option:
+                       # Help text: For more information on what qualifies as specified employment, check out these notes on the Immigration and Nationality Act, Section 319(b).
+                       ('3', "I’m applying based on my qualifying military service. "),
+                       ('4', "I’m applying for another reason."),
+                       )
 GENDER_OPTIONS = (BLANK_CHOICE, ('female', 'Female'), ('male', 'Male'))
+EYE_COLOR_CHOICES = (('0', 'black'),
+                     ('1', 'blue'),
+                     ('2', 'brown'),
+                     ('3', 'gray'),
+                     ('4', 'green'),
+                     ('5', 'hazel'),
+                     ('6', 'maroon'),
+                     ('7', 'pink'),
+                     ('8', 'unknown/other')
+                     )
+HAIR_COLOR_CHOICES = (('0', 'bald (no hair)'),
+                      ('1', 'black'),
+                      ('2', 'blonde'),
+                      ('3', 'brown'),
+                      ('4', 'gray'),
+                      ('5', 'red'),
+                      ('6', 'sandy'),
+                      ('7', 'white'),
+                      ('8', 'unknown/other')
+                      )
+
 STATES = ((BLANK_CHOICE,) + STATE_CHOICES)
 MARITAL_STATUS = (BLANK_CHOICE, ('single', 'Single'), ('married_or_remarried', 'Married or remarried'), ('separated', 'Separated'), ('divorced', 'Divorced'), ('widowed', 'Widowed'))
 
 
 class N400Step1(forms.Form):
-    dummy_question = True
+    a_number = forms.IntegerField(label="Your nine-digit A-number", help_text="Your A-number is the eight- or nine-digit number on your Permanent Resident Card.", required=False)
+    not_a_minor = forms.ChoiceField(label="Are you at least 18 years old?", choices=YES_OR_NO, widget=forms.RadioSelect, required=False)
+    why_eligible = forms.ChoiceField(label="How are you eligible to apply for citizenship?", choices=ELIGIBILITY_OPTIONS, widget=forms.RadioSelect, required=False)
+    # @todo: Reinstate that radio button issue.
 
 
 class N400Step2(forms.Form):
-    # "Your current legal name (*do not* provide a nickname)""
-    first_name = forms.CharField(label="Given name (First name)", required=False)
+    # "Your current legal name"
+    last_name = forms.CharField(label="Last name (family name)", required=False)
+    first_name = forms.CharField(label="First name (given name)", required=False)
     middle_name = forms.CharField(label="Middle name (if applicable)", required=False)
-    last_name = forms.CharField(label="Family name (Last name)", required=False)
-    # Your name exactly as it appears on your permanent resident card (if applicable)
-    # @todo: If we end up using both of these, find out which one should be carried through to the other form(s).
-#    card_first_name = forms.CharField(label="Given name (First name)", required=False)
-#    card_middle_name = forms.CharField(label="Middle name (if applicable)", required=False)
-#    card_last_name = forms.CharField(label="Family name (Last name)", required=False)
-    date_of_birth = forms.DateTimeField(label='Date of birth (MM/DD/YYYY)', required=False)
+    other_names = forms.CharField(label="Have you ever used any other names? If so, please list them", help_text="Include nicknames, aliases, and your maiden name, if applicable.", required=False)
+    # @todo: Determine whether to add "Would you like to legally change your name?" here or elsewhere.
+    ssn = USSocialSecurityNumberField(label="U.S. Social Security number (if you have one)", help_text="Why do we need this? We collect your Social Security number to verify your identity and protect you against fraud. We don’t store this information once we’ve processed your application for naturalization.", required=False)
+    # @todo: Determine whether that help text ^ is still accurate.
+    uscis_acct_number = forms.CharField(label="USCIS online account number (if you have one)", required=False)
+    # ^ If this were a live app, we'd add validation and more specifics. It
+    # isn't, so we won't spend time on that now.
     gender = forms.ChoiceField(choices=GENDER_OPTIONS, label="What is your gender?", required=False)
+    height_pt1 = forms.IntegerField(label="Height, feet", required=False)
+    height_pt2 = forms.IntegerField(label="Height, inches", required=False)
+    # @todo: Adjust the display of the above so they're reasonable.
+    weight = forms.IntegerField(label="Weight in pounds", required=False)
+    # @todo: Fix rendering of these checkboxes.
+    eye_color = forms.ChoiceField(label="Eye color", choices=EYE_COLOR_CHOICES, widget=forms.CheckboxSelect, required=False)
+    hair_color = forms.ChoiceField(label="Hair color", choices=HAIR_COLOR_CHOICES, widget=forms.CheckboxSelect, required=False)
+    date_of_birth = forms.DateTimeField(label='Date of birth (MM/DD/YYYY)', required=False)
+    date_of_residency = forms.DateTimeField(label='Date you became a Lawful Permanent Resident (MM/DD/YYYY)', required=False)
     country_of_birth = forms.CharField(label="Country of birth", required=False)
-    ssn = USSocialSecurityNumberField(label="Social Security number", help_text="Why do we need this? We collect your Social Security number to verify your identity and protect you against fraud. We don’t store this information once we’ve processed your application for naturalization.", required=False)
-    email = forms.EmailField(label="Email address", required=False)
-    # @todo: Tweak this to allow for non-U.S. phone numbers.
-    phone = USPhoneNumberField(label="Telephone number", required=False)
+    city_of_birth = forms.CharField(label="City of birth", required=False)
+    country_of_citizenship = forms.CharField(label="Country of citizenship or nationality", required=False)
+    # "Current residential address"
+    residential_address_street = forms.CharField(label="Street number and name", required=False)
+    residential_address_apt = forms.CharField(label="Apartment or floor number (if applicable)", required=False)
+    residential_address_city = forms.CharField(label="City", required=False)
+    residential_address_state = forms.ChoiceField(choices=STATES, label="State", required=False)
+    # @maybe: Refactor as a USStateSelect.
+    residential_address_zip = USZipCodeField(label="ZIP code", required=False)
+    # @maybe: Refactor these as CountryField()s.
+    # Docs: https://pypi.python.org/pypi/django-countries#countryselectwidget
+    residential_address_country = forms.CharField(label="Country, if outside the U.S.", required=False)
+    # @todo: Pick up here with dates of residence.
+
     mailing_address_permanent = forms.CharField(label="Permanent mailing address (incl. apt. number)", required=False)
     mailing_address_city = forms.CharField(label="City", required=False)
     mailing_address_state = forms.ChoiceField(choices=STATES, label="State", required=False)
     # @maybe: Refactor as a USStateSelect.
     mailing_address_zip = USZipCodeField(label="ZIP code", required=False)
-    # @maybe: Refactor these as CountryField()s.
-    # Docs: https://pypi.python.org/pypi/django-countries#countryselectwidget
-    country = forms.CharField(label="Country, if outside U.S.", required=False)
-    country_of_citizenship = forms.CharField(label="Country of citizenship", required=False)
+
+    email = forms.EmailField(label="Email address", required=False)
+    # @todo: Tweak this to allow for non-U.S. phone numbers.
+    phone = USPhoneNumberField(label="Telephone number", required=False)
     other_names = forms.CharField(label="Other names you have used since birth", required=False)
 
 
